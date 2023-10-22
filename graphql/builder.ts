d58764cd2a1bf4536fbe2357a537829ec01fcb3b
Othy Tenk <othy.tenk@gmail.com>
2023-10-22 18:54:18 +0800

    feat: configure the prisma and the graphql

---
+++ graphql/builder.ts
@@ -0,0 +1,28 @@
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import { DateTimeResolver } from "graphql-scalars";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+
+
+import { createContext } from "./context";
+import prisma from "@/prisma/prismadb";
+
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes;
+  Context: ReturnType<typeof createContext>;
+  Scalars: {
+    DateTime: { Input: Date; Output: Date };
+  };
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
+
+// in GraphQL the Query type and Mutation type can each only be called once. So we call it here and will add fields to them on the go
+builder.queryType();
+builder.mutationType();
+
+// This is where we've created the new date scalar
+builder.addScalarType("DateTime", DateTimeResolver, {});
