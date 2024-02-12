ffef61883a6fb99f21f4a2173edb3228fff46938
Othy Tenk <othy.tenk@gmail.com>
2024-02-12 17:50:48 +0800

    chore: setup schadcn-ui and create header component and page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const router = useRouter();
+
+  const onCreatePost = () => {
+    router.push("/create");
+  };
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="font-semibold">
+          <Link href="/">BlogQL</Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <Button variant="outline" onClick={onCreatePost}>
+            Create Post
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
